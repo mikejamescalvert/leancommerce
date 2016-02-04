cbeb18aa4a5f2f9dcb0d70c2d38ebe49988cdcbd
Mike Calvert <mike.calvert@issusa.com>
2016-02-04 09:19:13 -0800

    Added role assignment to admin user

--- src/LeanCommerce/Controllers/AdminController.cs
+++ src/LeanCommerce/Controllers/AdminController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using LeanCommerce.Abstract.Controllers;
+using Microsoft.AspNet.Authorization;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace LeanCommerce.Controllers
 {
+    [Authorize(Roles ="Admin")]
     public class AdminController : AbstractSetupRequiredController
     {
         public AdminController(Services.MongoSettings.Service.IMongoSettingsService mongoService) : base(mongoService)
--- src/LeanCommerce/Controllers/SetupController.cs
+++ src/LeanCommerce/Controllers/SetupController.cs
@@ -7,6 +7,9 @@ using LeanCommerce.ViewModels.Setup;
 using Microsoft.Extensions.OptionsModel;
 using LeanCommerce.Models;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc.Filters;
+using Microsoft.AspNet.Routing;
+using AspNet.Identity3.MongoDB;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,15 +21,18 @@ namespace LeanCommerce.Controllers
         Services.EncryptionSettings.Service.IEncryptionSettingsService _encryptionService;
         UserManager<ApplicationUser> _userManager;
         SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
         public SetupController(Services.MongoSettings.Service.IMongoSettingsService mongoService,
                                 Services.EncryptionSettings.Service.IEncryptionSettingsService encryptionService,
                                 UserManager<ApplicationUser> userManager,
+                                RoleManager<IdentityRole> roleManager,
                                 SignInManager<ApplicationUser> signInManager) : base()
         {
             _mongoService = mongoService;
             _encryptionService = encryptionService;
             _userManager = userManager;
             _signInManager = signInManager;
+            _roleManager = roleManager;
         }
         // GET: /<controller>/
         public IActionResult Index()
@@ -74,16 +80,41 @@ namespace LeanCommerce.Controllers
         {
             if (ModelState.IsValid == true)
             {
-                //todo: try to create an admin user
+                //try to create an admin user
                 try
                 {
                     
                     var user = new ApplicationUser { UserName = model.EmailAddress, Email = model.EmailAddress };
                     var result = await _userManager.CreateAsync(user, model.Password);
+                    
                     if (result.Succeeded)
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return RedirectToAction("SetupComplete");
+                        //assign user to new admin role
+                        var role = new IdentityRole { Name = "Admin" };
+                        var roleResult = await _roleManager.CreateAsync(role);
+                        if (roleResult.Succeeded)
+                        {
+                            var assignResult = await _userManager.AddToRoleAsync(user, "Admin");
+                            if (assignResult.Succeeded)
+                            {
+                                await _signInManager.SignInAsync(user, isPersistent: false);
+                                return RedirectToAction("SetupComplete");
+                            } else {
+                                foreach (var item in assignResult.Errors)
+                                {
+                                    ModelState.AddModelError("", item.Description);
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            foreach (var item in roleResult.Errors)
+                            {
+                                ModelState.AddModelError("", item.Description);
+                            }
+                        }
+
                     }
                     else
                     {
@@ -108,5 +139,17 @@ namespace LeanCommerce.Controllers
         {
             return View();
         }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (_mongoService != null && _mongoService.RequiresSetup() == false)
+                context.Result = new RedirectToRouteResult(
+                new RouteValueDictionary
+                    {{"controller", "Home"}, {"action", "Index"}});
+
+
+            base.OnActionExecuting(context);
+        }
+
     }
 }
